da38283b7d92a345b12d3a34ef89d59278b1235a
Mat <mathewvaleriano6@outlook.com>
2026-02-16 01:22:05 +0800

    feat: add react-native.config.js for autolinking native modules

---
+++ react-native.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  dependencies: {
+    'react-native-gesture-handler': {
+      platforms: {
+        android: {},
+      },
+    },
+    'react-native-reanimated': {
+      platforms: {
+        android: {},
+      },
+    },
+    'react-native-screens': {
+      platforms: {
+        android: {},
+      },
+    },
+    'react-native-safe-area-context': {
+      platforms: {
+        android: {},
+      },
+    },
+    'react-native-svg': {
+      platforms: {
+        android: {},
+      },
+    },
+    'react-native-webview': {
+      platforms: {
+        android: {},
+      },
+    },
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        android: {},
+      },
+    },
+    '@react-native-community/datetimepicker': {
+      platforms: {
+        android: {},
+      },
+    },
+    'react-native-worklets': {
+      platforms: {
+        android: {},
+      },
+    },
+  },
+};
