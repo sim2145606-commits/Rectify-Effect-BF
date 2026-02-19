08ef1a804d69ffac969e14553391fcb575d9ee1c
Mat <mathewvaleriano6@outlook.com>
2026-02-19 20:52:06 +0800

    Fix expo-file-system import in LogService

--- services/LogService.ts
+++ services/LogService.ts
@@ -1,5 +1,5 @@
 import { NativeModules, Platform } from 'react-native';
-import * as FileSystem from 'expo-file-system/legacy';
+import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import { getSystemInfo } from './SystemVerification';
 
