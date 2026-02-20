c0c482b280dc92fe4ecd8b7cd927df6b822cbf7a
Mat <mathewvaleriano6@outlook.com>
2026-02-21 01:14:32 +0800

    Fix Android build: Remove Expo wrapper classes causing unresolved references

--- android/app/src/main/java/com/briefplantrain/virtucam/MainActivity.kt
+++ android/app/src/main/java/com/briefplantrain/virtucam/MainActivity.kt
@@ -8,8 +8,6 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-import expo.modules.ReactActivityDelegateWrapper
-
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     setTheme(R.style.AppTheme)
@@ -27,11 +25,7 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
-    return ReactActivityDelegateWrapper(
-      this,
-      BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-    )
+    return DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
   }
 
   /**
